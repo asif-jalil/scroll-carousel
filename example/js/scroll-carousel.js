bb1a812d911d1bf5d9dc75464424d7c67f7ed2fb
emranhossain-wdd <emranhossain4005@gmail.com>
2022-10-30 09:40:40 +0600

    wip

--- example/js/scroll-carousel.js
+++ example/js/scroll-carousel.js
@@ -1,43 +1,66 @@
 (function(){
     let demo = document.querySelector('#mainBox1');
-    // function ScrollCarousel (element){
+    function ScrollCarousel (element){
         
-    //     this.element = element;
-    //     // console.log(this.element.innerHTML)
-    //     // kick things off
-    //     this._create();
-    // }
+        this.element = element;
+        // kick things off
+        this._create();
+    }
 
-    // ScrollCarousel.defaults = {};
+    ScrollCarousel.defaults = {};
 
-    // // hash of methods triggered on _create()
-    // ScrollCarousel.create = {};
+    // hash of methods triggered on _create()
+    ScrollCarousel.create = {};
 
-    // let proto = ScrollCarousel.prototype;
+    let proto = ScrollCarousel.prototype;
 
-    // proto._create = function() {
+    proto._create = function() {
 
-    //     // create slider
-    //     this._createSlider();
+        // create slider
+        this._createSlider();
 
-    //     this.something();
+        // add listeners from on option
 
-    //     // add listeners from on option
-    // }
 
-    // // slider positions the cells
-    // proto._createSlider = function() {
-    //     // slider element does all the positioning
-    //     let slider = document.createElement('div');
-    //     slider.className = 'scroll-carousel-slider';
-    //     this.slider = slider;
-    // };
+        this.activate();
+    }
 
-    // // append the slider into selected dom
-    // proto.something = function(){
-    //     console.log(this.element);
-    //     this.element.appendChild(this.slider);
-    // }
+    // slider positions the cells
+    proto._createSlider = function() {
+        // slider element does all the positioning
+        let slider = document.createElement('div');
+        slider.className = 'scroll-carousel-slider';
+        this.slider = slider;
+    };
+
+    proto.activate = function() {
+        if (this.isActive) return;
+
+        this.isActive = true;
+
+        // move initial cell elements so they can be loaded as cells
+        let cellElems = this._filterFindCellElements( this.element.children);
+        this.slider.append( ...cellElems );
+        this.element.append( this.slider );
+    }
+
+    proto._filterFindCellElements = function( elems ) {
+        return // something;
+    }
+
+    proto.isScrolledIntoView = function(el) {
+        if (!el) {
+          return false;
+        }
+        const rect = el.getBoundingClientRect();
+        const windowHeight =
+          window.innerHeight || document.documentElement.clientHeight;
+        const windowWidth =
+          window.innerWidth || document.documentElement.clientWidth;
+        const vertInView = rect.top <= windowHeight && rect.top + rect.height >= 0;
+        const horInView = rect.left <= windowWidth && rect.left + rect.width >= 0;
+        return vertInView && horInView;
+      }
 
     new ScrollCarousel(demo);
 })();
--- src/js/core.js
+++ src/js/core.js
@@ -1,41 +1,88 @@
-( function( window, factory ) {
-    let _ScrollCarousel = window.ScrollCarousel;
-
-    window.ScrollCarousel = factory( window );
-}( typeof window != 'undefined' ? window : this,
-function factory( window ){
-
-    // vars
-    // const { console } = window;
+function ScrollCarousel (element){
     
-    function ScrollCarousel (element){
-        
-        this.element = document.querySelector(element);
-        console.log(this.element)
-        // kick things off
-        this._create();
+    this.element = element;
+
+    // options
+    this.options = { ...this.constructor.defaults };
+    this.option( this.options );
+
+    // kick things off
+    this._create();
+}
+
+ScrollCarousel.defaults = {
+    friction: 0.055
+};
+
+// hash of methods triggered on _create()
+ScrollCarousel.create = {};
+
+let proto = ScrollCarousel.prototype;
+
+proto._create = function() {
+
+    // create slider
+    this._createSlider();
+
+    // add listeners from on option
+
+
+    this.activate();
+}
+
+/**
+ * set options
+ * @param {Object} opts - options to extend
+ */
+ proto.option = function( opts ) {
+    Object.assign( this.options, opts );
+};
+
+proto.activate = function() {
+    if (this.isActive) return;
+
+    this.isActive = true;
+    this._translate = 0;
+
+    // move initial cell elements so they can be loaded as cells
+    let cellElems = this._filterFindCellElements( this.element.children);
+    this.slider.append( ...cellElems );
+    this.element.append( this.slider );
+
+
+    window.addEventListener("scroll", function () {
+        if (this.isScrolledIntoView(this.element)) {
+            this.slider.style.transform = `translateX(${this._translate}%)`;
+            this._translate -= this.options.friction;
+            if (this._translate <= -50) {
+                this._translate = 0;
+            }
+        }
+    });
+}
+
+// slider positions the cells
+proto._createSlider = function() {
+    // slider element does all the positioning
+    let slider = document.createElement('div');
+    slider.className = 'scroll-carousel-slider';
+    this.slider = slider;
+};
+
+proto._filterFindCellElements = function( elems ) {
+    return // something;
+}
+
+proto.isScrolledIntoView = function(el) {
+    if (!el) {
+        return false;
     }
-
-    ScrollCarousel.defaults = {};
-
-    // hash of methods triggered on _create()
-    ScrollCarousel.create = {};
-
-    let proto = ScrollCarousel.prototype;
-
-    proto._create = function() {
-
-        // create slider
-        this._createSlider();
-
-        // add listeners from on option
-    }
-
-    // slider positions the cells
-    proto._createSlider = function() {
-        // slider element does all the positioning
-        let slider = document.createElement('div');
-        slider.className = 'scroll-carousel-slider';
-        this.slider = slider;
-    };
-}) );
+    const rect = el.getBoundingClientRect();
+    const windowHeight =
+        window.innerHeight || document.documentElement.clientHeight;
+    const windowWidth =
+        window.innerWidth || document.documentElement.clientWidth;
+    const vertInView = rect.top <= windowHeight && rect.top + rect.height >= 0;
+    const horInView = rect.left <= windowWidth && rect.left + rect.width >= 0;
+    return vertInView && horInView;
+}
