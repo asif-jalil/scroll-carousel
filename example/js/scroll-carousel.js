6396ab12d011e510fd7f7e5c431d87d6b63f591c
emranhossain-wdd <emranhossain4005@gmail.com>
2022-11-08 23:20:49 +0600

    example added

--- example/js/scroll-carousel.js
+++ example/js/scroll-carousel.js
@@ -1,7 +1,11 @@
 // import { ScrollCarousel } from './dist/scroll.carousel';
 (function () {
-  let demo = document.querySelector('#mainBox1');
+  let demo1 = document.querySelector('#demo1');
+  let demo2 = document.querySelector('#demo2');
 
-  new ScrollCarousel(demo);
+  new ScrollCarousel(demo1);
+  new ScrollCarousel(demo2, {
+    speed: 15
+  });
 })();
 
