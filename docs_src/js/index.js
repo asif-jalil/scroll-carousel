1fee6f8e866ea30a099a8dfa2d4d1186016cbd38
MostofaNobiTanveer <mntanveer9196@gmail.com>
2022-11-14 09:17:17 +0600

    docs options

--- docs_src/js/index.js
+++ docs_src/js/index.js
@@ -1,10 +1,8 @@
 import highlightjsInit from './highlight';
-import optionsInit from './options';
 import clipboardInit from './clipboard';
 
-
 // Get Document ready
-const docReady = (fn) => {
+const docReady = fn => {
   if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', fn);
   } else {
