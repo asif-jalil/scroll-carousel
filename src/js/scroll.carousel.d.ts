a8a55ca0a9fb0fb822f621466e827477919f49d4
Asif Jalil <62372647+asif-jalil@users.noreply.github.com>
2022-11-24 00:35:57 +0600

    ts file better commenting and destroy method added

--- src/js/scroll.carousel.d.ts
+++ src/js/scroll.carousel.d.ts
@@ -1,13 +1,34 @@
 declare class ScrollCarousel {
   constructor(element: Element | NodeList | string, options?: ScrollCarousel.Options);
+
+  /**
+   * Clears all bindings.
+   */
+  destroy(): void;
 }
 
 declare namespace ScrollCarousel {
   interface Options {
+    /**
+     * Movement speed of the carousel
+     */
     speed?: number;
+    /**
+     * Handle the speed according to acceleration
+     */
     smartSpeed?: boolean;
+    /**
+     * Margin between two slides
+     */
     margin?: number;
+    /**
+     * Slide will play auto
+     */
     autoplay?: boolean;
+    /**
+     * select slide with class name which you want to select for carousel.
+     *  other element will behave as simple
+     */
     slideSelector?: string;
   }
 }
